71c05dce95f622cd0d4ab74b0a1d0008726dfc43
mkko120 <mrmikillll@gmail.com>
2021-06-26 16:05:46 +0200

    s

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'f1b26be9e34c2925c0f07c99bcc9b36b97dd4090',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'f1b26be9e34c2925c0f07c99bcc9b36b97dd4090',
+            'dev_requirement' => false,
+        ),
+    ),
+);
